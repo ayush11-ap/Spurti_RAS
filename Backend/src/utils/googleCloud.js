95169fc40fb27d22a7dc118cfa2b03f34993d196
Ayush Pawar <ayushap1105@gmail.com>
2025-04-06 18:16:05 +0530

    Fix keyFilename path for Google Cloud Storage initialization

--- Backend/src/utils/googleCloud.js
+++ Backend/src/utils/googleCloud.js
@@ -1,7 +1,7 @@
 const { Storage } = require("@google-cloud/storage");
 
 const storage = new Storage({
-  keyFilename: "etc/secrets/spurti.json",
+  keyFilename: "/etc/secrets/spurti.json",
 });
 
 const bucket = storage.bucket("image_video_document_storage");
